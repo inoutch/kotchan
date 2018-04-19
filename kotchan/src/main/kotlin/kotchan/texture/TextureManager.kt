9ed39bb4de95472910c9a9c13f3aac72ae3c15c2
inoutch <akiandaki@gmail.com>
2018-04-19 18:07:50 +0900

    Add square and camera creator

--- kotchan/src/main/kotlin/kotchan/texture/TextureManager.kt
+++ kotchan/src/main/kotlin/kotchan/texture/TextureManager.kt
@@ -2,6 +2,8 @@ package kotchan.texture
 
 import interop.graphic.GL
 import interop.graphic.GLTexture
+import kotchan.scene.drawable.Square
+import utility.type.Vector2
 
 class TextureManager(private val gl: GL) {
     private val textureMap = mutableMapOf<String, GLTexture>()
@@ -11,6 +13,11 @@ class TextureManager(private val gl: GL) {
             return GLTexture.empty
         }
         return textureMap[filepath] ?: gl.loadTexture(filepath) ?: GLTexture.empty
+    }
+
+    fun createSquare(filepath: String): Square {
+        val texture = get(filepath)
+        return Square(Vector2(texture.width.toFloat(), texture.height.toFloat()))
     }
 
     fun clearAll() {
