a5f6f7d3fb9816dd67eed2d438ea113b6041a79b
illush123 <al14028@shibaura-it.ac.jp>
2018-05-03 20:13:12 +0900

    add android touch

--- kotchan-android/src/main/kotlin/com/example/app/Renderer.kt
+++ kotchan-android/src/main/kotlin/com/example/app/Renderer.kt
@@ -12,20 +12,19 @@ import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 
-class Renderer(context: Context?): GLSurfaceView.Renderer{
+class Renderer(private val context: Context?): GLSurfaceView.Renderer{
 
-    private var context = context
     private var engine : Engine? = null
     private var beforeMillis: Long = 0
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        var winMan : WindowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        var display = winMan.defaultDisplay
-        var dispMet = DisplayMetrics()
+        val winMan : WindowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = winMan.defaultDisplay
+        val dispMet = DisplayMetrics()
         display.getRealMetrics(dispMet)
         val width = dispMet.widthPixels
         val height = dispMet.heightPixels
-        engine = Engine()
+        engine = Engine.getInstance()
         engine?.init(Vector2(width.toFloat(), height.toFloat()), AppConfig.SCREEN_SIZE)
         GLES30.glClearColor(1.0f,0.0f,0.0f,1.0f)
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT or GLES30.GL_DEPTH_BUFFER_BIT)
@@ -36,10 +35,6 @@ class Renderer(context: Context?): GLSurfaceView.Renderer{
     }
 
     override fun onDrawFrame(gl: GL10?) {
-        val millisPerFrame = System.currentTimeMillis() - beforeMillis
-        beforeMillis = System.currentTimeMillis()
-        engine?.draw(millisPerFrame.toFloat() / 1000.0f)
-        assert( GLES30.glGetError() == GLES30.GL_NO_ERROR ){
-        }
+        engine?.draw()
     }
 }
