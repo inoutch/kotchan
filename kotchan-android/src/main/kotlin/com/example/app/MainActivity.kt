f82a72b172530f160817357ecac3d03c50bd4b14
illush123 <al14028@shibaura-it.ac.jp>
2018-05-07 14:14:31 +0900

    refactor android

--- kotchan-android/src/main/kotlin/com/example/app/MainActivity.kt
+++ kotchan-android/src/main/kotlin/com/example/app/MainActivity.kt
@@ -4,18 +4,17 @@ import android.content.res.AssetManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-
-
 class MainActivity : AppCompatActivity() {
     companion object {
         private var assetManager: AssetManager? = null
         fun getAssets() = assetManager
     }
-    private var mySurfaceView: SurfaceView? = null
+
+    private lateinit var surfaceView: SurfaceView
     override fun onCreate(savedInstanceState: Bundle?) {
         assetManager = assets
-        mySurfaceView = SurfaceView(this)
+        surfaceView = SurfaceView(this)
         super.onCreate(savedInstanceState)
-        setContentView(mySurfaceView)
+        setContentView(surfaceView)
     }
 }
--- kotchan-android/src/main/kotlin/com/example/app/SurfaceView.kt
+++ kotchan-android/src/main/kotlin/com/example/app/SurfaceView.kt
@@ -3,95 +3,82 @@ package com.example.app
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.MotionEvent
 import android.view.WindowManager
+import application.AppConfig
 import kotchan.Engine
 import kotchan.controller.touch.TouchEvent
 
 import utility.type.Vector2
 
 
-class SurfaceView(context: Context?) : GLSurfaceView(context)
-{
+class SurfaceView(context: Context?) : GLSurfaceView(context) {
     private val touchEvents = mutableMapOf<Int, TouchEvent>()
     private var engine: Engine = Engine()
-    private var width : Float = 0.0f
-    private var height : Float = 0.0f
+    private var width: Float
+    private var height: Float
+
     init {
         setEGLContextClientVersion(3)
-        val myRenderer = Renderer(context)
-        setRenderer(myRenderer)
-        val winMan : WindowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        val winMan: WindowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val display = winMan.defaultDisplay
         val dispMet = DisplayMetrics()
         display.getRealMetrics(dispMet)
         width = dispMet.widthPixels.toFloat()
         height = dispMet.heightPixels.toFloat()
-    }
-    private fun debug(dlist: List<TouchEvent>){
-        for((index,touchEvent) in dlist.withIndex()){
-            Log.d("debug","index=$index,x=${touchEvent.point.x},y=${touchEvent.point.y}")
-        }
+        engine.init(Vector2(width, height), AppConfig.SCREEN_SIZE)
+        setRenderer(Renderer(engine))
     }
 
-    override fun onTouchEvent(e: MotionEvent): Boolean {
-        val eventAction = e.actionMasked
-        when(eventAction){
-            MotionEvent.ACTION_DOWN,MotionEvent.ACTION_POINTER_DOWN -> {
-                val pointerIndex = e.actionIndex
-                val x = e.getX(pointerIndex)
-                val y = e.getY(pointerIndex)
-                val point= Vector2(x,height-y)
-                val pointerid = e.getPointerId(pointerIndex)
-                val touchEvent = TouchEvent(point)
-                touchEvents[pointerid] = touchEvent
-            //    debug(touchEvents.values.toList())
-            //    engine.touchEmitter.onTouchesBegan(touchEvents.values.toList())}
-                engine.touchEmitter.onTouchesBegan(listOf(touchEvent))
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-                val historySize = e.historySize
-                for(h in 0 until historySize) {
-                    val historyList = touchEvents.map { (pointerId, touchEvent) ->
-                        val pointerIndex = e.findPointerIndex(pointerId)
-                        val x = e.getHistoricalX(pointerIndex,h)
-                        val y = e.getHistoricalY(pointerIndex,h)
-                        touchEvent.point = Vector2(x, height-y)
-                        touchEvent
-                    }
-             //       debug(historyList)
-                    engine.touchEmitter.onTouchesMoved(historyList)
-                }
-                val currentList = touchEvents.map{(pointerId,touchEvent) ->
-                    val pointerIndex = e.findPointerIndex(pointerId)
-                    val x = e.getX(pointerIndex)
-                    val y = e.getY(pointerIndex)
-                    touchEvent.point = Vector2(x,height-y)
-                    touchEvent
-                }
-             //   debug(currentList)
-                engine.touchEmitter.onTouchesMoved(currentList)
-            }
-            MotionEvent.ACTION_UP,MotionEvent.ACTION_POINTER_UP -> {
-                val pointerIndex = e.actionIndex
-                val x = e.getX(pointerIndex)
-                val y = e.getY(pointerIndex)
-                val pointerId = e.getPointerId(pointerIndex)
-                val touchEvent = touchEvents[pointerId] ?: return false
-                touchEvent.point = Vector2(x,height-y)
-             //   debug(touchEvents.values.toList())
-            //    engine.touchEmitter.onTouchesEnded(touchEvents.values.toList())}
-                engine.touchEmitter.onTouchesEnded(listOf(touchEvent))
-                touchEvents -= pointerId
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                touchEvents.clear()
-                engine.touchEmitter.onTouchesCancelled()
-            }
-
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val eventAction = event.actionMasked
+        when (eventAction) {
+            MotionEvent.ACTION_DOWN,
+            MotionEvent.ACTION_POINTER_DOWN -> this.onTouchesBegan(event)
+            MotionEvent.ACTION_MOVE -> this.onTouchesMoved(event)
+            MotionEvent.ACTION_UP,
+            MotionEvent.ACTION_POINTER_UP -> this.onTouchesEnded(event)
+            MotionEvent.ACTION_CANCEL -> this.onTouchesCancelled()
         }
         return true
     }
+
+    private fun onTouchesBegan(event: MotionEvent) {
+        val list = eventsToList(event).map {
+            val touchEvent = TouchEvent(it.second)
+            touchEvents[it.first] = touchEvent
+            touchEvent
+        }
+        engine.touchEmitter.onTouchesBegan(list)
+    }
+
+    private fun onTouchesMoved(event: MotionEvent) {
+        val list = eventsToList(event).mapNotNull {
+            touchEvents[it.first]
+        }
+        engine.touchEmitter.onTouchesMoved(list)
+    }
+
+    private fun onTouchesEnded(event: MotionEvent) {
+        val list = eventsToList(event).mapNotNull {
+            touchEvents[it.first]
+        }
+        engine.touchEmitter.onTouchesEnded(list)
+    }
+
+    private fun onTouchesCancelled() {
+        engine.touchEmitter.onTouchesCancelled()
+        touchEvents.clear()
+    }
+
+    private fun eventsToList(event: MotionEvent): List<Pair<Int, Vector2>> {
+        val list = mutableListOf<Pair<Int, Vector2>>()
+        val count = event.pointerCount
+        for (i in 0 until count) {
+            list.add(Pair(event.getPointerId(i), Vector2(event.getX(i), height - event.getY(i))))
+        }
+        return list
+    }
+
 }
