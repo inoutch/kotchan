cd5a1b4832f93d9c9d0a648be7187c039292ae70
inoutch <akiandaki@gmail.com>
2018-05-12 23:47:36 +0900

    Fixed not to apply initialized color to mock button

--- kotchan/src/main/kotlin/kotchan/view/ui/MockButton.kt
+++ kotchan/src/main/kotlin/kotchan/view/ui/MockButton.kt
@@ -9,6 +9,10 @@ import utility.type.Vector4
 
 class MockButton(camera: Camera, size: Vector2, onClick: () -> Unit) : Square(size) {
     var normalColor = Vector4(0.5f, 0.5f, 1.0f, 1.0f)
+        set(value) {
+            field = value
+            color = value
+        }
     var pressedColor = Vector4(0.2f, 0.2f, 1.0f, 1.0f)
 
     init {
