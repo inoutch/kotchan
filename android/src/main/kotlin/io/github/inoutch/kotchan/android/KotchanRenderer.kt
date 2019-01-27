544da640e4d17cbe038abf77e117e312fa8659c7
Akinori Inoue <akiandaki@gmail.com>
2019-01-27 17:35:07 +0900

    fix(kotchan-jvm, kotchan-android): fixed to synchronize for touch event (#25)

--- android/src/main/kotlin/io/github/inoutch/kotchan/android/KotchanRenderer.kt
+++ android/src/main/kotlin/io/github/inoutch/kotchan/android/KotchanRenderer.kt
@@ -9,16 +9,20 @@ import io.github.inoutch.kotchan.utility.type.Point
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
-class KotchanRenderer(private val config: KotchanEngine.Config, val windowManager: WindowManager) : GLSurfaceView.Renderer {
+class KotchanRenderer(
+        private val config: KotchanEngine.Config,
+        private val windowManager: WindowManager) : GLSurfaceView.Renderer {
+
     lateinit var core: KotchanCore
 
-    override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
+    override fun onSurfaceCreated(gl: GL10, config: EGLConfig) = synchronized(this) {
         val display = windowManager.defaultDisplay
         val dispMet = DisplayMetrics()
         display.getRealMetrics(dispMet)
 
         core = KotchanCore(this.config, Point(dispMet.widthPixels, dispMet.heightPixels))
         core.init()
+        return@synchronized
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) = core.reshape(0, 0, width, height)
