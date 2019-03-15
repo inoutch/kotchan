4dbc1f5c835d3d778f6e8693a37b830eb5b5ed38
unknown <akiandaki@gmail.com>
2019-03-15 12:36:37 +0900

    fix(kotchan): fixed update sampler

--- src/commonMain/kotlin/io.github.inoutch/kotchan/utility/graphic/vulkan/helper/VKSampler.kt
+++ src/commonMain/kotlin/io.github.inoutch/kotchan/utility/graphic/vulkan/helper/VKSampler.kt
@@ -10,15 +10,15 @@ class VKSampler(private val descriptorSets: List<VkDescriptorSet>, private val b
     fun setTexture(vk: VK, texture: Texture) {
         val bundle = texture.vkTexture ?: throw VkInvalidStateError("VKSampler")
         if (texture != this.texture) {
-            val imageInfo = VkDescriptorImageInfo(
-                    bundle.sampler, bundle.imageView, VkImageLayout.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
-            val descriptorWrite = VkWriteDescriptorSet(
-                    descriptorSets[vk.currentImageIndex],
-                    binding,
-                    0,
-                    VkDescriptorType.VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-                    listOf(imageInfo), listOf(), listOf())
-            vkUpdateDescriptorSets(vk.device, listOf(descriptorWrite), listOf())
+            val descriptorWrites = vk.commandBuffers.mapIndexed { index, _ ->
+                val imageInfo = VkDescriptorImageInfo(
+                        bundle.sampler, bundle.imageView, VkImageLayout.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+                VkWriteDescriptorSet(
+                        descriptorSets[index], binding, 0,
+                        VkDescriptorType.VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+                        listOf(imageInfo), listOf(), listOf())
+            }
+            vkUpdateDescriptorSets(vk.device, descriptorWrites, listOf())
             this.texture = texture
         }
     }
