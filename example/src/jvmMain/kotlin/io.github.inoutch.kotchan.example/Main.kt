81789ab4f51ab40f91c4a289900996761ab78b4c
inoutch <akiandaki@gmail.com>
2019-07-06 12:28:52 +0900

    fix(kotchan-example): fixed to run jvm

--- example/src/jvmMain/kotlin/io.github.inoutch.kotchan.example/Main.kt
+++ example/src/jvmMain/kotlin/io.github.inoutch.kotchan.example/Main.kt
@@ -1,11 +1,7 @@
 package io.github.inoutch.kotchan.example
 
-import kotlin.jvm.JvmStatic
 import io.github.inoutch.kotchan.core.KotchanEngine
 
-object Main {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        KotchanEngine(AppConfig()).run()
-    }
+fun main(args: Array<String>) {
+    KotchanEngine(AppConfig()).run()
 }
