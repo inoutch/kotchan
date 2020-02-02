c189d4f6cf4bf706ef60c58c602ad23a98daf7d7
inoutch <akiandaki@gmail.com>
2020-02-02 21:33:33 +0900

    feat(kotchan): Add flag to use Vulkan API preferentially in application config

--- core/src/commonMain/kotlin/io/github/inoutch/kotchan/core/KotchanConfig.kt
+++ core/src/commonMain/kotlin/io/github/inoutch/kotchan/core/KotchanConfig.kt
@@ -1,12 +1,15 @@
 package io.github.inoutch.kotchan.core
 
+import io.github.inoutch.kotchan.core.graphic.compatible.vk.VKContext
 import io.github.inoutch.kotchan.math.Vector2I
 import kotlin.native.concurrent.ThreadLocal
 
-class KotchanConfig {
+class KotchanConfig(platform: KotchanPlatform) {
     @ThreadLocal
     val windowSize: Vector2I = Vector2I.Zero
 
     @ThreadLocal
     val viewportSize: Vector2I = Vector2I.Zero
+
+    val useVulkan = platform.graphic is VKContext
 }
