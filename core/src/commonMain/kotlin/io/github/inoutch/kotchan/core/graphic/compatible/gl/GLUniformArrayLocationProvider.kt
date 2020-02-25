2024b522842f6003ed2c185c148663c1866cf67e
inoutch <akiandaki@gmail.com>
2020-02-25 23:51:38 +0900

    fix(kotchan): Format

--- core/src/commonMain/kotlin/io/github/inoutch/kotchan/core/graphic/compatible/gl/GLUniformArrayLocationProvider.kt
+++ core/src/commonMain/kotlin/io/github/inoutch/kotchan/core/graphic/compatible/gl/GLUniformArrayLocationProvider.kt
@@ -3,9 +3,9 @@ package io.github.inoutch.kotchan.core.graphic.compatible.gl
 import io.github.inoutch.kotlin.gl.api.gl
 
 class GLUniformArrayLocationProvider(
-        shader: GLShader,
-        uniformName: String,
-        size: Int
+    shader: GLShader,
+    uniformName: String,
+    size: Int
 ) {
     val locations = List(size) { gl.getUniformLocation(shader.program, "$uniformName[$it]") }
 }
